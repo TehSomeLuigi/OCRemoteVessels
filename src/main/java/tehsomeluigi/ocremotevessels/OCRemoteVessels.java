8e6e5ec6d79ed649865f3f56125c7022147f355c
TehSomeLuigi <olivier.the.olive@gmail.com>
2014-07-16 21:50:30 +0100

    Initial commit.

---
+++ src/main/java/tehsomeluigi/ocremotevessels/OCRemoteVessels.java
@@ -0,0 +1,25 @@
+package tehsomeluigi.ocremotevessels;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import tehsomeluigi.ocremotevessels.reference.Reference;
+
+@Mod(modid=Reference.MOD_ID, version="dev")
+public class OCRemoteVessels {
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent evt) {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent evt) {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent evt) {
+
+    }
+}
---
+++ src/main/java/tehsomeluigi/ocremotevessels/reference/Reference.java
@@ -0,0 +1,5 @@
+package tehsomeluigi.ocremotevessels.reference;
+
+public class Reference {
+    public static final String MOD_ID = "ocremotevessels";
+}
